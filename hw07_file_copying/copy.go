3049453713de44de653b09f9e1c97ccafecd4193
dk <dima9380@yandex.ru>
2023-01-14 17:35:12 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,83 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrWrongOffsetParam      = errors.New("wrong offset")
+	ErrWrongLimitParam       = errors.New("wrong limit")
+	ErrWrongFromParam        = errors.New("empty from file path")
+	ErrWrongToParam          = errors.New("empty to file path")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if fromPath == "" {
+		return ErrWrongFromParam
+	}
+	if toPath == "" {
+		return ErrWrongToParam
+	}
+	if offset < 0 {
+		return ErrWrongOffsetParam
+	}
+	if limit < 0 {
+		return ErrWrongLimitParam
+	}
+
+	var err error
+	var fileFrom *os.File
+	if fileFrom, err = os.Open(fromPath); err != nil {
+		return fmt.Errorf("copy: %w", err)
+	}
+	defer fileFrom.Close()
+
+	var fi fs.FileInfo
+	if fi, err = fileFrom.Stat(); err != nil {
+		return fmt.Errorf("copy: %w", err)
+	}
+
+	if !fi.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	fromFileSize := fi.Size()
+	remainSize := fromFileSize - offset
+	if remainSize <= 0 {
+		return ErrOffsetExceedsFileSize
+	}
+
+	needSize := offset + limit
+	if limit == 0 || needSize > fromFileSize {
+		limit = remainSize
+	}
+
+	if _, err = fileFrom.Seek(offset, io.SeekStart); err != nil {
+		return fmt.Errorf("copy: %w", err)
+	}
+
+	var fileTo *os.File
+	if fileTo, err = os.Create(toPath); err != nil {
+		return fmt.Errorf("copy: %w", err)
+	}
+	defer fileTo.Close()
+
+	bar := pb.Full.Start64(limit)
+	defer bar.Finish()
+
+	bar.Set(pb.Bytes, true)
+
+	barReader := bar.NewProxyReader(fileFrom)
+	if _, err = io.CopyN(fileTo, barReader, limit); err != nil && !errors.Is(err, io.EOF) {
+		return fmt.Errorf("copy: %w", err)
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,95 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/udhos/equalfile"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	f, err := os.CreateTemp("", "")
+	if err != nil {
+		require.NoError(t, err)
+	}
+
+	from := "./testdata/input.txt"
+	to := f.Name()
+	cmp := equalfile.New(nil, equalfile.Options{})
+
+	defer os.Remove(to)
+
+	t.Run("empty from", func(t *testing.T) {
+		err := Copy("", to, 0, 0)
+		require.Error(t, err)
+	})
+
+	t.Run("empty to", func(t *testing.T) {
+		err = Copy(from, "", 0, 0)
+		require.Error(t, err)
+	})
+
+	t.Run("wrong offset", func(t *testing.T) {
+		err = Copy(from, to, -1, 0)
+		require.Error(t, err)
+	})
+
+	t.Run("wrong limit", func(t *testing.T) {
+		err = Copy(from, to, 0, -1)
+		require.Error(t, err)
+	})
+
+	t.Run("copy file from to", func(t *testing.T) {
+		err := Copy(from, to, 0, 0)
+		require.NoError(t, err)
+		equal, err := cmp.CompareFile("./testdata/out_offset0_limit0.txt", to)
+		require.NoError(t, err)
+		require.True(t, equal)
+	})
+
+	t.Run("copy no exist file", func(t *testing.T) {
+		fakeFrom := "./testdata/input_fake.txt"
+		err = Copy(fakeFrom, to, 0, 0)
+		require.Error(t, err)
+	})
+
+	t.Run("copy set limit", func(t *testing.T) {
+		err := Copy(from, to, 0, 10)
+		require.NoError(t, err)
+		equal, err := cmp.CompareFile("./testdata/out_offset0_limit10.txt", to)
+		require.NoError(t, err)
+		require.True(t, equal)
+	})
+
+	t.Run("copy set limit over file", func(t *testing.T) {
+		err := Copy(from, to, 0, 10000)
+		require.NoError(t, err)
+		equal, err := cmp.CompareFile("./testdata/out_offset0_limit10000.txt", to)
+		require.NoError(t, err)
+		require.True(t, equal)
+	})
+
+	t.Run("copy set offset", func(t *testing.T) {
+		err := Copy(from, to, 100, 1000)
+		require.NoError(t, err)
+		equal, err := cmp.CompareFile("./testdata/out_offset100_limit1000.txt", to)
+		require.NoError(t, err)
+		require.True(t, equal)
+	})
+
+	t.Run("copy set offset over file", func(t *testing.T) {
+		err := Copy(from, to, 100000, 0)
+		require.Error(t, err)
+	})
+
+	t.Run("copy not regular files", func(t *testing.T) {
+		notRegularFile := "/dev/urandom"
+		err = Copy(notRegularFile, to, 0, 0)
+		require.Error(t, err)
+
+		notRegularFile = "/dev/zero"
+		err = Copy(notRegularFile, to, 0, 0)
+		require.Error(t, err)
+	})
 }
